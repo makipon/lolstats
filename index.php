712e109748cac26bf9d4d9e6b842ad7fcbfc48d8
makipon <krabbymcpatty@gmail.com>
2018-06-19 17:38:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 $servername = "127.0.0.1";
 $username = "root";
 $password = "eskimos5";
+$dbname = "db_lolstats";
 
 //create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 //check connection
 if ($conn->connect_error) {
@@ -12,5 +13,36 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 echo "Hello from Sites Folder";
+
+$sql = "SELECT * FROM champion";
+if($result = $mysqli->query($sql)){
+    if($result->num_rows > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                echo "<th>name</th>";
+                echo "<th>role</th>";
+				echo "<th>tier</th>";
+				echo "<th>win rate</th>";
+            echo "</tr>";
+        while($row = $result->fetch_array()){
+            echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['role'] . "</td>";
+				echo "<td>" . $row['tier'] . "</td>";
+				echo "<td>" . $row['win rate'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        $result->free();
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+}
+
 ?>
 
